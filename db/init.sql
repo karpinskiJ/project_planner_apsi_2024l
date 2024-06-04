715e956e6c9ee9a1112985d028f6691483060f9a
jkarpins <01150169@pw.edu.pl>
2024-06-04 20:22:07 +0200

    Calendar added, database schema changed to assign multiple projects to user.

--- db/init.sql
+++ db/init.sql
@@ -1,28 +1,6 @@
 CREATE DATABASE project_planner;
 \c project_planner;
 
-
-
-CREATE TYPE project_status AS ENUM ('pending', 'in_progress','completed');
-CREATE TABLE projects(
-    id serial NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    status project_status NOT NULL,
-    CONSTRAINT pk_projects PRIMARY KEY (id)
-    --CONSTRAINT fk_owner FOREIGN KEY ( owner_id ) REFERENCES users ( "id" )
-);
-
---INIT DATA
-INSERT INTO projects (name, description, start_date, end_date, status)
-VALUES
-('Project A', 'Description of Project A', '2024-06-01', '2024-06-30', 'in_progress'),
-('Project B', 'Description of Project B', '2024-06-15', '2024-08-31', 'pending'),
-('Project C', 'Description of Project C', '2024-03-20', '2024-06-01', 'completed');
-
-
 CREATE TYPE project_role AS ENUM ('manager', 'worker','admin');
 CREATE TABLE users(
 	id        serial NOT NULL,
@@ -35,7 +13,6 @@ CREATE TABLE users(
 	setup_time timestamp NOT NULL,
 	CONSTRAINT pk_users PRIMARY KEY ( "id" )
 );
-
 INSERT INTO users (login, password, name, surname, role, manager_id, setup_time) VALUES
 ('manager1', '$2b$12$HDuQW0Vp97aVjBY4LBgltO2Hn6sPKtyw92A2BoNzStEjolgr6SwLm', 'John', 'Doe', 'manager', NULL, NOW()),
 ('worker1', '$2b$12$HDuQW0Vp97aVjBY4LBgltO2Hn6sPKtyw92A2BoNzStEjolgr6SwLm', 'Jane', 'Smith', 'worker', 1, NOW()),
@@ -44,6 +21,31 @@ INSERT INTO users (login, password, name, surname, role, manager_id, setup_time)
 ('manager2', '$2b$12$HDuQW0Vp97aVjBY4LBgltO2Hn6sPKtyw92A2BoNzStEjolgr6SwLm', 'Robert', 'Brown', 'manager', NULL, NOW()),
 ('worker3', '$2b$12$HDuQW0Vp97aVjBY4LBgltO2Hn6sPKtyw92A2BoNzStEjolgr6SwLm', 'Michael', 'Davis', 'worker', 5, NOW()),
 ('admin2', '$2b$12$HDuQW0Vp97aVjBY4LBgltO2Hn6sPKtyw92A2BoNzStEjolgr6SwLm', 'Jessica', 'Garcia', 'admin', NULL, NOW());
+
+CREATE TYPE project_status AS ENUM ('pending', 'in_progress','completed');
+CREATE TABLE projects(
+    id serial NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status project_status NOT NULL,
+    owner_id integer NOT NULL,
+    CONSTRAINT pk_projects PRIMARY KEY (id),
+    CONSTRAINT fk_owner FOREIGN KEY ( owner_id ) REFERENCES users ( "id" )
+);
+
+--INIT DATA
+INSERT INTO projects (name, description, start_date, end_date, status,owner_id)
+VALUES
+('Project A', 'Description of Project A', '2024-06-01', '2024-06-30', 'in_progress',1),
+('Project B', 'Description of Project B', '2024-06-15', '2024-08-31', 'pending',1),
+('Project C', 'Description of Project C', '2024-03-20', '2024-06-01', 'completed',5);
+
+
+
+
+
 
 
 
